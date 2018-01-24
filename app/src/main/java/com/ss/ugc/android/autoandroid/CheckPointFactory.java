c41f69c74254a8c8ca32ed8faf709063d90eceff
tongyao <tongyao@bytedance.com>
2018-01-24 21:54:20 +0800

    add element gone

--- app/src/main/java/com/ss/ugc/android/autoandroid/CheckPointFactory.java
+++ app/src/main/java/com/ss/ugc/android/autoandroid/CheckPointFactory.java
@@ -2,6 +2,7 @@ package com.ss.ugc.android.autoandroid;
 
 import com.ss.ugc.android.autoandroid.checkpoints.AbsCheckPoint;
 import com.ss.ugc.android.autoandroid.checkpoints.EleExistCP;
+import com.ss.ugc.android.autoandroid.checkpoints.EleGoneCP;
 
 /**
  * Created by tongyao on 2018/1/5.
@@ -12,6 +13,8 @@ public final class CheckPointFactory {
         switch (name) {
             case "ele_exist":
                 return new EleExistCP(params);
+            case "ele_gone":
+                return new EleGoneCP(params);
             default:
                 return null;
         }
